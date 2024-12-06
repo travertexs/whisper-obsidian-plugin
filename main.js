5092dece49b35ad43936930b06ac93fa0fe0e87d
MNoichl <noichlmax@hotmail.co.uk>
2024-12-06 21:32:55 +0100

    updated name

--- main.js
+++ main.js
@@ -2716,10 +2716,10 @@ var DEFAULT_SETTINGS = {
   createNewFileAfterRecordingPath: "",
   // Set defaults for new settings
   usePostProcessing: true,
-  postProcessingPrompt: "You are a perfect transcription program that is able to take faulty dictations and put them into a readable, grammatically correct form without changing their content or changing their specific formulations. It is important that you leave formulations as they are, and make no attempts to formalize or professionalize them. If the dictation is a todolist, write it as a todolist in markdown format. If there are repetions of content, choose the best (often last) one and make the sentence work with that. Here comes the dictation: \n\n",
+  postProcessingPrompt: "You are a perfect transcription program that is able to take faulty dictations and put them into a readable, grammatically correct form without changing their content or changing their specific formulations. It is important that you leave formulations as they are, and make no attempts to formalize or professionalize them. If the dictation is a todolist, write it as a todolist in markdown format. If there are repetions of content, choose the best (often last) one and make the sentence work with that. Always transcribe in the language of the dictation. Here comes the dictation: \n\n",
   postProcessingModel: "gpt-4o",
   autoGenerateTitle: true,
-  titleGenerationPrompt: "Generate a short, precise title for the following text. Reply only with the title, nothing else. Here is the text:"
+  titleGenerationPrompt: "Generate a short, precise title for the following text. Reply only with the title, nothing else. Generate the title in the main language of the text.Here is the text:"
 };
 var SettingsManager = class {
   constructor(plugin) {
